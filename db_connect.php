6901b47d99d431cc3c2a16e7de347d2a77e30faf
Beaurval <beaury.valentin@gmail.com>
2021-01-17 23:38:10 +0100

    first commit

---
+++ db_connect.php
@@ -0,0 +1,6 @@
+<?php
+$server = "mysql-beaurval.alwaysdata.net";
+$username = "beaurval";
+$password = "alwayscesi";
+$db = "beaurval_lotoapp";
+$conn = mysqli_connect($server, $username, $password, $db);
